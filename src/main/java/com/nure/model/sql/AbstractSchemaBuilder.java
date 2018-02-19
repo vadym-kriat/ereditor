da272ba86267de5d433e3186543ab15ec25c577c
vadym-kriat <kriat.vadym@gmail.com>
2018-02-19 20:26:07 +0200

    Add abstract schema builder and SQLite schema builder.

--- src/main/java/com/nure/model/sql/AbstractSchemaBuilder.java
+++ src/main/java/com/nure/model/sql/AbstractSchemaBuilder.java
@@ -3,27 +3,38 @@ package com.nure.model.sql;
 import com.nure.model.schema.Schema;
 import com.nure.model.schema.table.Table;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Vadim_ on 18.02.2018.
  */
 public abstract class AbstractSchemaBuilder implements SchemaBuilder {
-    private String name;
-    private List<Table> tables;
+    protected String name;
+    protected List<Table> tables;
+
+    protected static final String SPACE = " ";
+    protected static final String LINE_SEP = System.lineSeparator();
+
+    protected AbstractSchemaBuilder() {
+        this.tables = new ArrayList<>();
+    }
 
     @Override
     public void forScheme(Schema schema) {
-
+        this.name = schema.getName();
+        this.tables = schema.listOfTables();
     }
 
     @Override
     public SchemaBuilder setName(String name) {
-        return null;
+        this.name = name;
+        return this;
     }
 
     @Override
     public SchemaBuilder addTable(Table table) {
-        return null;
+        this.tables.add(table);
+        return this;
     }
 }
---
+++ src/main/java/com/nure/model/sql/sqlite/SQLiteShemeBuilder.java
@@ -0,0 +1,25 @@
+package com.nure.model.sql.sqlite;
+
+import com.nure.model.sql.AbstractSchemaBuilder;
+import com.nure.model.sql.TableBuilder;
+
+import java.util.stream.Collectors;
+
+/**
+ * Created by Vadim_ on 19.02.2018.
+ */
+public class SQLiteShemeBuilder extends AbstractSchemaBuilder {
+
+    @Override
+    public String buildQuery() {
+        StringBuilder separator = new StringBuilder();
+        for (int i = 0; i < 2; i++) {
+            separator.append(LINE_SEP);
+        }
+        return String.join(separator, tables.stream().map(table -> {
+            TableBuilder tableBuilder = new SQLiteTableBuilder();
+            tableBuilder.forTable(table);
+            return tableBuilder.buildQuery();
+        }).collect(Collectors.toList()));
+    }
+}
