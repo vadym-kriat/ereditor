44438df74237fd904c234f3a8696823e3ca71d38
vadym-kriat <kriat.vadym@gmail.com>
2018-02-20 12:28:00 +0200

    Fix bug.

--- src/main/java/com/nure/model/sql/sqlite/SQLiteQueryBuilderFactory.java
+++ src/main/java/com/nure/model/sql/sqlite/SQLiteQueryBuilderFactory.java
@@ -5,7 +5,7 @@ import com.nure.model.sql.*;
 /**
  * Created by Vadim_ on 17.02.2018.
  */
-class SQLiteQueryBuilderFactory implements QueryBuilderFactory {
+public class SQLiteQueryBuilderFactory implements QueryBuilderFactory {
     @Override
     public SchemaBuilder getSchemeBuilder() {
         return new SQLiteSchemaBuilder();
