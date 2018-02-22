4e707edba399f2e402454cd1e5a5aadcbbd74a5e
vadym-kriat <kriat.vadym@gmail.com>
2018-02-22 17:24:05 +0200

    Implement load and save methods.

--- src/main/java/com/nure/model/io/SchemaFileManager.java
+++ src/main/java/com/nure/model/io/SchemaFileManager.java
@@ -1,6 +1,14 @@
 package com.nure.model.io;
 
 import com.nure.model.schema.Schema;
+import com.nure.model.schema.table.Column;
+import com.nure.model.schema.table.ForeignKey;
+import com.nure.model.schema.table.Table;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import java.io.File;
 
 /**
  * Created by Vadim_ on 22.02.2018.
@@ -13,10 +21,20 @@ public class SchemaFileManager {
     }
 
     public void save(Schema schema) throws Exception {
-
+        File file = new File(pathname);
+        JAXBContext context = JAXBContext.newInstance(
+                Schema.class, Table.class, Column.class, ForeignKey.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(schema, file);
     }
 
     public Schema load() throws Exception {
-        return null;
+        File file = new File(pathname);
+        JAXBContext context = JAXBContext.newInstance(
+                Schema.class, Table.class, Column.class, ForeignKey.class);
+        Unmarshaller unmarshaller = context.createUnmarshaller();
+        Schema schema = (Schema) unmarshaller.unmarshal(file);
+        return schema;
     }
 }
